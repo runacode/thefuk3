7b3b97438b0c64eac0e0d3378314b76431209450
runacode <59107019+runacode@users.noreply.github.com>
2020-02-12 16:52:34 -0600

    Initial commit

---
+++ includes/description.php
@@ -0,0 +1,296 @@
+
+<h2 class="featured-in text-uppercase text-center mt-3"><span><?= T('Featured In'); ?></span></h2>
+<div class="logos mt-5">
+    <img class="img-fluid" src="../resources/images/logos.jpg">
+</div>
+<div class="three-benefites three-benefited__count-four d-flex justify-content-between mt-5">
+    <div>
+        <img class src="../resources/images/allergy-free-icon.png">
+        <div><?= T('Allergy'); ?><br><?= T('Free'); ?></div>
+    </div>
+    <div>
+        <img class src="../resources/images/100-natural-icon.png">
+        <div><?= T('Microwave'); ?><br><?= T('Safe'); ?></div>
+    </div>
+    <div>
+        <img class src="../resources/images/clinically-tested-icon.png">
+        <div><?= T('Clinically'); ?><br><?= T('Tested'); ?></div>
+    </div>
+    <div>
+        <img class src="../resources/images/fda-icon.png">
+        <div><?= T('FDA'); ?><br><?= T('Approved'); ?></div>
+    </div>
+</div>
+
+<div class="pain-point text-uppercase mt-5"><?= T('BIG EYELASHES IN 3 WEEKS'); ?><br><?= T('OR YOUR MONEY BACK!'); ?>
+</div>
+<div class="featuring text-uppercase text-center"><?= T('Featuring'); ?></div>
+<img class="img-fluid" src="../resources/images/fegserum-feature-logo.jpg">
+
+<div class="benefit text-center mt-5"><?= T('GET SEXY, LONG EYE LASHES IN JUST WEEKS!'); ?><Br><span
+            class="highlighted"><?= T('LOOK BETTER THAN EVER - WITHOUT DANGEROUS'); ?></span><br> <?= T('EXTENSIONS THAT RUIN YOUR NATURAL EYELASHES.'); ?><Br>
+    <span class="highlighted"><b><?= T('EYELASHES GROW 20 - 40% AFTER THE FIRST 7-14 DAYS!'); ?></b></span></div>
+
+
+<p class="gif-animation mt-5 text-center" style="margin-top: 10px;">
+    <video class autoplay muted loop playsinline poster="../resources/images/transparent-pixel.png">
+        <source type="video/mp4" src="../resources/images/FEG_LP_01.mp4">
+    </video>
+</p>
+
+<div class="benefit mt-4">
+    <div class="text-center"><span
+                class="highlighted"><?= T('SAVE UP TO $45 - $50 a MONTH ($600/year) '); ?></span><br><?= T('WITH LONG, BEAUTIFUL'); ?> <span
+                class="highlighted"><?= T('NATURAL'); ?></span> <?= T('EYELASHES'); ?></div>
+</div>
+
+<div class="magic-grid mt-5">
+
+    <div class="d-flex flex-row">
+        <div>
+
+            <img class="img-fluid" src="../resources/images/feg-d-week-1.jpg">
+        </div>
+        <div class="pink-rounded"><?= T('WEEK 1'); ?></div>
+    </div>
+
+    <div class="d-flex flex-row mt-3">
+        <div>
+
+            <img class="img-fluid" src="../resources/images/feg-d-week-2.jpg">
+        </div>
+        <div class="pink-rounded"><?= T('WEEK 2'); ?></div>
+    </div>
+
+    <div class="d-flex flex-row mt-3">
+        <div>
+
+            <img class="img-fluid" src="../resources/images/feg-d-week-3.jpg">
+        </div>
+        <div class="pink-rounded"><?= T('WEEK 3'); ?></div>
+    </div>
+
+</div>
+
+<div class="gif-animation mt-5 text-center">
+    <video class autoplay muted loop playsinline poster="../resources/images/transparent-pixel.png">
+        <source type="video/mp4" src="../resources/images/FEG_LP_02.mp4">
+    </video>
+</div>
+
+<div class="magic-grid mt-5">
+
+    <div class="d-flex flex-row">
+        <div>
+
+            <img class="img-fluid" src="../resources/images/100-natural-img.jpg">
+        </div>
+        <div>100% <br><?= T('NATURAL'); ?></div>
+    </div>
+
+    <div class="d-flex flex-row">
+        <div><?= T('EASY TO APPLY'); ?><br><?= T('AT NIGHT'); ?></div>
+        <div>
+            <img class="img-fluid" src="../resources/images/night-treatment-img.jpg" alt></div>
+    </div>
+
+    <div class="d-flex flex-row">
+        <div>
+            <img class="img-fluid" src="../resources/images/big-strong-eyelashes-img.jpg" alt></div>
+        <div><?= T('BIG & BEAUTIFUL'); ?><br><?= T('EYELASHES'); ?></div>
+    </div>
+
+    <div class="d-flex flex-row">
+        <div><?= T('DOES NOT'); ?><br><?= T('IRRITATE EYES'); ?></div>
+        <div>
+            <img class="img-fluid" src="../resources/images/irritate-eyes-img.jpg" alt></div>
+    </div>
+
+    <div class="d-flex flex-row">
+        <div>
+            <img class="img-fluid" src="../resources/images/tested-animals-img.jpg" alt></div>
+        <div><?= T('NOT TESTED'); ?><br><?= T('ON ANIMALS'); ?></div>
+    </div>
+</div>
+
+<div class="red-arrow-next-line">
+    <img src="../resources/images/red_arrow_next_line.svg" alt>
+</div>
+
+<div class="below-red-arrow-text text-justify mt-5"><?= T('Save hundreds of dollars with your own beautiful lashes - no fragrances, no dangerous chemicals, and non-toxic. 100% and <u>never</u> tested on animals!'); ?></div>
+
+
+<div class="daily_treatment_section mt-5">
+    <div class="row">
+        <div class="col-12 text-center" data-aos="fade-right">
+            <h5><?= T('FAST AND EASY'); ?></h5>
+            <h6><?= T('apply at night'); ?></h6>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3 text-center" data-aos="fade-right">
+            <img src="../resources/images/daily-treatment-1.jpg" class="img-fluid" alt="Arrow">
+            <h4><?= T('Apply at'); ?><br><?= T('the root'); ?></h4>
+        </div>
+        <div class="col-3 text-center" data-aos="fade-right">
+            <img src="../resources/images/daily-treatment-2.jpg" class="img-fluid" alt="Arrow">
+            <h4><?= T('Week 1'); ?></h4>
+        </div>
+        <div class="col-3 text-center" data-aos="fade-right">
+            <img src="../resources/images/daily-treatment-3.jpg" class="img-fluid" alt="Arrow">
+            <h4><?= T('Week 2'); ?></h4>
+        </div>
+        <div class="col-3 text-center" data-aos="fade-right">
+            <img src="../resources/images/daily-treatment-4.jpg" class="img-fluid" alt="Arrow">
+            <h4><?= T('Week 3'); ?></h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12" data-aos="fade-right">
+
+
+            <p>
+            <?= T('"Fake eyelashes are so uncomfortable, and everyone can tell you\'re lying. But now I have my own, real eyelashes, and they look amazing! I couldn\'t believe how fast they grew."'); ?>
+
+            </p>
+            <h3 class="text-right"><?= T('Hannah Johnson'); ?></h3>
+            <div class="text-center">
+                <img src="../resources/images/eyes-img.png" class="img-fluid" alt="Eyes">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="pain-point text-uppercase mt-5"><?= T('MEET SOME OF OUR'); ?><br><?= T('SUCCESS STORIES'); ?></div>
+
+<div class="before_after_section mt-5">
+    <div class="row">
+        <div class="col-12 text-center" data-aos="fade-right">
+            <div class="ba_html_grid">
+                <div>
+                    <img src="../resources/images/ba_01.jpg" alt="Eyes" class="lazy">
+                    <div class="text">
+                        <div class="top"><?= T('BEFORE'); ?></div>
+                        <div class="bottom"></div>
+                    </div>
+                </div>
+                <div>
+                    <img src="../resources/images/ba_02.jpg" alt="Eyes" class="lazy">
+                    <div class="text">
+                        <div class="top"><?= T('AFTER'); ?></div>
+                        <div class="bottom">
+                            <div><?= T('Angelina, 28'); ?></div>
+                            <div><?= T('Results after using for 7 weeks'); ?></div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img src="../resources/images/ba_03.jpg" alt="Eyes" class="lazy">
+                    <div class="text">
+                        <div class="top"><?= T('BEFORE'); ?></div>
+                        <div class="bottom"></div>
+                    </div>
+                </div>
+                <div>
+                    <img src="../resources/images/ba_04.jpg" alt="Eyes" class="lazy">
+                    <div class="text">
+                        <div class="top"><?= T('AFTER'); ?></div>
+                        <div class="bottom">
+                            <div><?= T('Maria, 21'); ?></div>
+                            <div><?= T('Results after using for 5 weeks'); ?></div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img src="../resources/images/ba_05.jpg" alt="Eyes" class="lazy">
+                    <div class="text">
+                        <div class="top"><?= T('BEFORE'); ?></div>
+                        <div class="bottom"></div>
+                    </div>
+                </div>
+                <div>
+                    <img src="../resources/images/ba_06.jpg" alt="Eyes" class="lazy">
+                    <div class="text">
+                        <div class="top"><?= T('AFTER'); ?></div>
+                        <div class="bottom">
+                            <div><?= T('Sunny, 32'); ?></div>
+                            <div><?= T('Results after using for 6 weeks'); ?></div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img src="../resources/images/ba_07.jpg" alt="Eyes" class="lazy">
+                    <div class="text">
+                        <div class="top"><?= T('BEFORE'); ?></div>
+                        <div class="bottom"></div>
+                    </div>
+                </div>
+                <div>
+                    <img src="../resources/images/ba_08.jpg" alt="Eyes" class="lazy">
+                    <div class="text">
+                        <div class="top"><?= T('AFTER'); ?></div>
+                        <div class="bottom">
+                            <div><?= T('Jessica, 36'); ?></div>
+                            <div><?= T('Results after using for 8 weeks'); ?></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="guaranteed-satisfaction mt-5">
+    <div class="text-center">
+        <img src="../resources/images/guarantee-seal.png" alt>
+    </div>
+    <div class="mt-5"><?= T('Our products are backed by a risk-free'); ?>
+        <em><?= T('30-day money-back'); ?></em><?= T('guarantee If you are not completely satisfied with your purchase'); ?>
+        <em><?= T('for ANY REASON'); ?></em>,<?= T('we will do'); ?>
+        <em><?= T('WHATEVER it takes'); ?></em><?= T('to make it right'); ?></div>
+</div>
+
+<h2 class="featured-in text-uppercase text-center mt-5"><span><?= T('Overall Rating'); ?></span></h2>
+
+<?php include 'includes/stamped-reviews.php' ?>
+
+<!--/.rocket-description-->
+
+<div class="container">
+    <div class="accordion" id="accordionExample">
+        <div class="card">
+            <div class="card-header" id="headingOne">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                            data-target="#collapseOne" aria-expanded="false"
+                            aria-controls="collapseOne"><?= T('FAQ'); ?></button>
+                </h2>
+            </div>
+
+            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                <div class="card-body">
+                    <?php include 'faq.php' ?>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                            data-target="#collapseTwo" aria-expanded="false"
+                            aria-controls="collapseTwo"><?= T('Contact Us'); ?></button>
+                </h2>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                <div class="card-body">
+                    <?php include 'contact-us.php' ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
